7fce472968e3d25c837d65d2a712e78792ed47b7
emmanuel-kipkiru <emmanuelkipkirui17@gmail.com>
2022-07-19 11:03:15 +0300

    event handling lab assign done

--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
 // Code Keypad Component Here
+import React, { PureComponent } from 'react'
+
+export default class Keypad extends PureComponent {     
+  render() {
+
+    function handleChange(event) {
+        console.log("Entering password...");
+    }
+
+    return (
+      <div>
+          <input type="password" name='password' placeholder='password' onChange={handleChange}/>
+      </div>
+    )
+  }
+}
